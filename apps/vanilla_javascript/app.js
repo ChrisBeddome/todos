4e69a906b38baebd6c1e2492259d2306f3be87b0
Chris Beddome <chrisbeddome@gmail.com>
2022-10-19 23:24:34 -0400

    initial javascript_vanilla commit

---
+++ apps/vanilla_javascript/app.js
@@ -0,0 +1,61 @@
+class Todo {
+	#completed = false;
+	#text = "";
+
+	constructor(text = "") {
+		this.#text = text.toString();
+	}
+
+	get text() {
+		return this.#text;
+	}
+
+	set text(text = "") {
+		this.#text = text.toString();
+	}
+
+	isComplete() {
+		return this.#completed;
+	}
+
+	markComplete() {
+		this.#completed = true;
+	}
+
+	markIncomplete() {
+		this.#completed = false;
+	}
+
+	toString() {
+		return this.#text;
+	}
+}
+
+class TodoList {
+	static #count = 0;
+	#name = "";
+	#todos = [];
+
+	constructor(name = "", ...args) {
+		if (name) {
+			this.#name = name.toString();
+		} else {
+			this.#name = `Todo List #${++TodoList.#count}`;
+		}
+		args.flat().forEach(todo => {
+			this.add(todo);
+		});
+	}
+
+	get todos() {
+		return this.#todos;
+	}
+	
+	add(todo) {
+		if (todo instanceof Todo)  {
+			this.#todos = [...this.#todos, todo];
+		} else {
+			throw "argument must be instance of Todo"
+		}
+	}
+}
